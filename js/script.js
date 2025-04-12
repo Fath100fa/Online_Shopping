13f6d9af0e848d832ab99118c9a07386370df872
Fath100fa <fathallahsamir88@gmail.com>
2025-04-12 21:29:39 +0200

    make  a checkbox password in register page for password and confirm password

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ function showPassword() {
     x.type = "password";
     }
 }
+function showPassword2() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+    x.type = "text";
+    } else {
+    x.type = "password";
+    }
+}
+function showPassword3() {
+    var x = document.getElementById("confirm_password");
+    if (x.type === "password") {
+    x.type = "text";
+    } else {
+    x.type = "password";
+    }
+}
