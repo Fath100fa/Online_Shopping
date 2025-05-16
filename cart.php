71e6902a0c3e0c062a72362305839de66eb7fe00
Fath100fa <fathallahsamir88@gmail.com>
2025-05-16 17:07:47 +0300

    added logout in add cart and profile and delete register

--- cart.php
+++ cart.php
@@ -17,11 +17,20 @@ if (!$conn) {
 <body>
     <nav>
         <h3><i class="fas fa-laptop-code"></i> CompuVerse</h3>
-        <ul>
+        <ul> 
+            <li><a href="profile.php">
+                <i class="fas fa-user"></i>
+                <?php
+                    if (isset($_SESSION["username"])) {
+                        echo $_SESSION["username"];
+                    } else {
+                        echo "Guest";
+                    }
+                ?>
+            </a></li>
             <li><a href="index.php"><i class="fas fa-home"></i> Home</a></li>
-            <li><a href="Login.php"><i class="fas fa-sign-in-alt"></i> Login</a></li>
-            <li><a href="Register.php"><i class="fas fa-user-plus"></i> Register</a></li>
-        </ul>
+            <li><a href="login.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                </ul>
     </nav>
 
     <h1><i class="fas fa-shopping-cart"></i> Shopping Cart</h1>
